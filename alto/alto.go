d604bc3fd99b8fc2e7b52e42d6a0f854bb36a092
john dowling <john.dowling@diskarchive.com>
2025-05-08 10:36:20 +0100

    Adding constructor to alto

--- alto/alto.go
+++ alto/alto.go
@@ -1,31 +1,48 @@
 package alto
 
 import (
+	"errors"
 	"fmt"
 	"github.com/Disk-Archive/alto-III-go/http"
 	"github.com/Disk-Archive/alto-III-go/meta_data"
-	"net"
 )
 
 type AltoIII struct {
-	IpAddress net.IP
-	Port      int
+	Hostname string
+	Port     int
 
 	IgnoreSslErrors bool
 
 	MetaData *meta_data.MetaData
 }
 
+func New(hostname string, port int, ignoreSsl bool) (altoIII *AltoIII, err error) {
+	if port < 0 || port > 65535 {
+		return nil, errors.New("port out of range 0-65535")
+	}
+
+	return &AltoIII{
+		Hostname:        hostname,
+		Port:            port,
+		IgnoreSslErrors: ignoreSsl,
+		MetaData: &meta_data.MetaData{
+			Object: &meta_data.Object{
+				Hostname: hostname,
+			},
+		},
+	}, nil
+}
+
 func (a *AltoIII) ArchiveObject(diskId, objectName, md5 string, data []byte) (err error) {
 	_, err = http.Post[interface{}](
-		a.IpAddress.String(), fmt.Sprintf("/api/v1/copy/archive/object/%s?location", diskId, objectName), data,
+		a.Hostname, fmt.Sprintf("/api/v1/copy/archive/object/%s?location", diskId, objectName), data,
 	)
 	return
 }
 
 func (a *AltoIII) DeleteObject(diskId, objectName string) (err error) {
 	_, err = http.Delete[interface{}](
-		a.IpAddress.String(), fmt.Sprintf("/api/v1/copy/archive/object/%s?location", diskId, objectName),
+		a.Hostname, fmt.Sprintf("/api/v1/copy/archive/object/%s?location", diskId, objectName),
 	)
 	return
 }
