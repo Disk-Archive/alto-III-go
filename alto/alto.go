d0a5e0226cce3674c5fe84a57c99612b7b89d224
john dowling <john.dowling@diskarchive.com>
2025-05-08 12:00:17 +0100

    fixing delete and copy method locations

--- alto/alto.go
+++ alto/alto.go
@@ -35,14 +35,14 @@ func New(hostname string, port int, ignoreSsl bool) (altoIII *AltoIII, err error
 
 func (a *AltoIII) ArchiveObject(diskId, objectName, md5 string, data []byte) (err error) {
 	_, err = http.Post[interface{}](
-		a.Hostname, fmt.Sprintf("/api/v1/copy/archive/object/%s?location", diskId, objectName), data,
+		a.Hostname, fmt.Sprintf("/api/v1/copy/archive/object/%s?location=", diskId, objectName), data,
 	)
 	return
 }
 
 func (a *AltoIII) DeleteObject(diskId, objectName string) (err error) {
 	_, err = http.Delete[interface{}](
-		a.Hostname, fmt.Sprintf("/api/v1/copy/archive/object/%s?location", diskId, objectName),
+		a.Hostname, fmt.Sprintf("/api/v1/copy/delete/%s?location=", diskId, objectName),
 	)
 	return
 }
