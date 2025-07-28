a49f3b8315831845d62844885103e5254f7df3b3
muneeb.ahmed <muneeb.ahmed@diskarchive.com>
2025-07-28 15:14:19 +0100

    ALTO-858 Create File functions in SDK ALTO-862 Add disk functions to SDK

---
+++ disk/constants.go
@@ -0,0 +1,17 @@
+package disk
+
+type RemoveStatus string
+
+const (
+	RemoveRequested RemoveStatus = "REMOVE_REQUESTED"
+	ReadyToRemove   RemoveStatus = "READY_TO_REMOVE"
+	Removed         RemoveStatus = "REMOVED"
+)
+
+type DiskHealthStatus string
+
+const (
+	DiskOk    DiskHealthStatus = "ok"
+	DiskWarn  DiskHealthStatus = "warn"
+	DiskError DiskHealthStatus = "error"
+)
